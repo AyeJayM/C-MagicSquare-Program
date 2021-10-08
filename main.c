74e8e8dfcecd8abbd3ba56f26172c74d53160315
Austin Martinez <austinm@cpp.edu>
2021-10-07 22:51:13 -0700

    FINAL VERSION

--- main.c
+++ main.c
@@ -40,7 +40,120 @@ void main()
         printf("\nThe array is NOT a Magic Square\n");
     }
 
+
+
+
+
+
+    /////////////////////////////////////////////////////
+
+    //Magic Square Generator
+
+    time_t t; //Volatile Variable allows for True Random
+
+    srand((unsigned)time(&t)); //Set random generation based on time.
+
+    int RandomResult = 9; //Holds result of Square Test on Generated Square
+    int CreatedSquares = 0; //Counter that contains how many squares we have made.
+
+
+    do 
+    {
+    //Now we assign each location in our square a random value form 1-9 with NO duplicates.
+
+    int position1 = ( (rand() % 9) + 1); //This gives a range of possible values from 0 - 9
+
+    int position2 = ( (rand() % 10) + 1);
+    while (position2 == position1)
+    {
+        position2 = ( (rand() % 10) + 1); //Reroll for a new number. No duplicates!
+    }
+
+    int position3 = ( (rand() % 10) + 1);
+    while (position3 == position2 || position3 == position1)
+    {
+        position3 = ( (rand() % 10) + 1); //Reroll for a new number. No duplicates!
+    }
+
+    int position4 = ( (rand() % 10) + 1);
+    while (position4 == position3 || position4 == position2 || position4 == position1)
+    {
+        position4 = ( (rand() % 10) + 1); //Reroll for a new number. No duplicates!
+    }
+
+    int position5 = ( (rand() % 10) + 1);
+    while (position5 == position4 || position5 == position3 || position5 == position2 ||
+                        position5 == position1)
+    {
+        position5 = ( (rand() % 10) + 1); //Reroll for a new number. No duplicates!
+    }
+
+    int position6 = ( (rand() % 10) + 1);
+    while (position6 == position5 || position6 == position4 || position6 == position3 ||
+                        position6 == position2 || position6 == position1)
+    {
+        position6 = ( (rand() % 10) + 1); //Reroll for a new number. No duplicates!
+    }
+
+    int position7 = ( (rand() % 10) + 1);
+    while (position7 == position6 || position7 == position5 || position7 == position4 ||
+                        position7 == position3 || position7 == position2 || position7 == position1)
+    {
+        position7 = ( (rand() % 10) + 1); //Reroll for a new number. No duplicates!
+    }
+
+    int position8 = ( (rand() % 10) + 1);
+    while (position8 == position7 || position8 == position6 || position8 == position5 || position8 == position4 ||
+                        position8 == position3 || position8 == position2 || position8 == position1)
+    {
+        position8 = ( (rand() % 10) + 1); //Reroll for a new number. No duplicates!
+    }
+
+    int position9 = ( (rand() % 10) + 1);
+    while (position9 == position8 || position9 == position7 || position9 == position6 ||
+                        position9 == position5 || position9 == position4 || position9 == position3 ||
+                        position9 == position2 || position9 == position1)
+    {
+        position9 = ( (rand() % 10) + 1); //Reroll for a new number. No duplicates!
+    }
+
+    //Assemble the Square with the previously Generated Values
+
+    int RandomSquare[3][3] =   { {position1, position2, position3 },
+                                 {position4, position5, position6 },
+                                 {position7, position8, position9 } };
+
+    CreatedSquares++;
+    printf("\nSquares generated: %d\n", CreatedSquares);
+
+    RandomResult = ( MagicTest(RandomSquare) );
+
+    if(RandomResult == 1)
+    {
+        printf("\nSquares Generated: %d\n", CreatedSquares);
+        printf("\nThe succesful square is shown below...\n");
+
+        printf("\n[%d %d %d]\n"
+                 "[%d %d %d]\n"
+                 "[%d %d %d]",  position1, position2, position3,
+                                position4, position5, position6,
+                                position7, position8, position9);
+    }
+
+    }
+    while(RandomResult == 0);
+    
+    
+    printf("\nEnding Program...\n");
+
 }
+
+
+
+
+
+
+////////////////////////////////////////////////////
 
 int MagicTest(int TestSquare[3][3])
 {
